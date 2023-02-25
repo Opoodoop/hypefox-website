3a03fbfbce9968aa1638cb50aee4de97f6dfb9a6
Exotical <ItzExotical@gmail.com>
2023-02-25 02:10:39 +0000

    Fix routing mistake

--- components/hero.vue
+++ components/hero.vue
@@ -11,7 +11,7 @@
                 <h1 class="text-white text-4xl sm:text-5xl md:text-6xl font-bold text-center mb-8 md:mb-12">Innovative IT-solutions for businesses and projects</h1>
                 <div class="w-full flex flex-col sm:flex-row sm:justify-center gap-2.5">
                    <NuxtLink to="/portfolio" class="inline-block bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3">Portfolio</NuxtLink>
-                   <NuxtLink href="/about" class="inline-block bg-gray-200 hover:bg-gray-300 focus-visible:ring ring-indigo-300 text-gray-500 active:text-gray-700 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3">About</NuxtLink>
+                   <NuxtLink to="/about" class="inline-block bg-gray-200 hover:bg-gray-300 focus-visible:ring ring-indigo-300 text-gray-500 active:text-gray-700 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3">About</NuxtLink>
                 </div>
              </div>
           </section>
