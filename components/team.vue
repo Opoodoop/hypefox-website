bc90fb3a9f9599138e553229d5e52e9dfccdc9ab
ItzExotical <ItzExotical@gmail.com>
2023-03-14 23:33:20 +0100

    Minor fixes, additions and improvements

--- components/team.vue
+++ components/team.vue
@@ -1,27 +1,47 @@
 <template>
    <div class="bg-gray-900">
-       <div class="max-w-screen-xl px-4 md:px-8 mx-auto">
+       <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
            <div class="mb-10 md:mb-16">
                <h2 class="text-white text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-6">Meet our team</h2>
                <p class="max-w-screen-md text-gray-400 md:text-lg text-center mx-auto">Our talented team members are the driving force behind our success. From creative designers to technical developers, our global team brings diverse perspectives and expertise to our company. Get to know the people behind our company and learn about their stories, goals, and aspirations.</p>
            </div>
-           <h2 class="text-white text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-6">Executives/board</h2>
-           <div class="flex flex-wrap justify-center items-center gap-x-4 lg:gap-x-8 gap-y-8 lg:gap-y-12">
+           <h2 class="text-white text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-6">Board members</h2>
+           <div class="flex flex-wrap justify-center overflow-x-auto gap-x-8 gap-y-12">
                <div class="bg-gray-800 pb-5 rounded-2xl items-center justify-center text-center">
-                   <div class="h-48 sm:h-60 md:h-80 overflow-hidden rounded-lg mb-2 sm:mb-4">
+                   <div class="overflow-hidden rounded-lg mb-2 sm:mb-4">
                        <img src="/img/emilio.png" class="w-[400px] h-[400px] object-center" />
                    </div>
                    <div>
                        <div class="text-indigo-500 md:text-lg font-bold">
                            <div class="flex items-center text-center justify-center gap-x-2 lg:gap-x-2"><img src="/img/sweden.png" class="h-[20px] w-[30px]" /> Emilio P.</div>
                        </div>
-                       <p class="text-gray-400 text-sm md:text-base mb-2 md:mb-2">Founder / Owner</p>
+                       <p class="text-gray-500 text-sm md:text-base">ItzExotical</p>
+                       <p class="text-gray-400 text-sm md:text-base mb-2">Founder / Owner</p>
                        <div class="flex items-center justify-center">
                            <div class="flex gap-4">
+                                <a href="https://github.com/ItzExotical" target="_blank" class="text-gray-400 hover:text-gray-500 active:text-gray-600 transition duration-100">
+                                   <Icon name="fa6-brands:github" class="h-5 w-5" />
+                               </a>
                                <a href="https://www.linkedin.com/in/emilio-persson-a4989b235" target="_blank" class="text-gray-400 hover:text-gray-500 active:text-gray-600 transition duration-100">
                                    <Icon name="fa6-brands:linkedin-in" class="h-5 w-5" />
                                </a>
-                               <a href="https://github.com/ItzExotical" target="_blank" class="text-gray-400 hover:text-gray-500 active:text-gray-600 transition duration-100">
+                           </div>
+                       </div>
+                   </div>
+               </div>
+               <div class="bg-gray-800 pb-5 rounded-2xl items-center justify-center text-center">
+                   <div class="overflow-hidden rounded-lg mb-2 sm:mb-4">
+                       <img src="/img/knut.png" class="w-[400px] h-[400px] object-center" />
+                   </div>
+                   <div>
+                       <div class="text-indigo-500 md:text-lg font-bold">
+                           <div class="flex items-center text-center justify-center gap-x-2 lg:gap-x-2"><img src="/img/sweden.png" class="h-[20px] w-[30px]" /> Knut L.</div>
+                       </div>
+                       <p class="text-gray-500 text-sm md:text-base">Opoodoop</p>
+                       <p class="text-gray-400 text-sm md:text-base mb-2 md:mb-2">Co-founder / CTO</p>
+                       <div class="flex items-center justify-center">
+                           <div class="flex gap-4">
+                               <a href="https://github.com/Opoodoop" target="_blank" class="text-gray-400 hover:text-gray-500 active:text-gray-600 transition duration-100">
                                    <Icon name="fa6-brands:github" class="h-5 w-5" />
                                </a>
                            </div>
@@ -29,19 +49,17 @@
                    </div>
                </div>
                <div class="bg-gray-800 pb-5 rounded-2xl items-center justify-center text-center">
-                   <div class="h-48 sm:h-60 md:h-80 overflow-hidden rounded-lg shadow-lg mb-2 sm:mb-4">
+                   <div class="overflow-hidden rounded-lg shadow-lg mb-2 sm:mb-4">
                        <img src="/img/lazar.png" class="w-[400px] h-[400px] object-center" />
                    </div>
                    <div>
                        <div class="text-indigo-500 md:text-lg font-bold">
                            <div class="flex items-center text-center justify-center gap-x-2 lg:gap-x-2"><img src="/img/romania.png" class="h-[20px] w-[30px]" /> Lazar S.</div>
                        </div>
-                       <p class="text-gray-400 text-sm md:text-base mb-2 md:mb-2">CFO / Owner</p>
+                       <p class="text-gray-500 text-sm md:text-base">ExoticalCat</p>
+                       <p class="text-gray-400 text-sm md:text-base mb-2 md:mb-2">Owner / CFO</p>
                        <div class="flex items-center justify-center">
                            <div class="flex gap-4">
-                               <a href="#" target="_blank" class="text-gray-400 hover:text-gray-500 active:text-gray-600 transition duration-100">
-                                   <Icon name="fa6-brands:linkedin-in" class="h-5 w-5" />
-                               </a>
                                <a href="https://github.com/devil38" target="_blank" class="text-gray-400 hover:text-gray-500 active:text-gray-600 transition duration-100">
                                    <Icon name="fa6-brands:github" class="h-5 w-5" />
                                </a>
@@ -53,21 +71,42 @@
            <h2 class="text-white text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-6 mt-10 md:mt-16">Management</h2>
            <div class="flex flex-wrap justify-center items-center gap-x-4 lg:gap-x-8 gap-y-8 lg:gap-y-12">
                <div class="bg-gray-800 pb-5 rounded-2xl items-center justify-center text-center">
-                   <div class="h-48 sm:h-60 md:h-80 overflow-hidden rounded-lg mb-2 sm:mb-4">
+                   <div class="overflow-hidden rounded-lg mb-2 sm:mb-4">
                        <img src="/img/liam.png" class="w-[400px] h-[400px] object-center" />
                    </div>
                    <div>
                        <div class="text-indigo-500 md:text-lg font-bold">
                            <div class="flex items-center text-center justify-center gap-x-2 lg:gap-x-2"><img src="/img/sweden.png" class="h-[20px] w-[30px]" /> Liam J.</div>
                        </div>
-                       <p class="text-gray-400 text-sm md:text-base mb-2 md:mb-2">CEO / Marketing</p>
+                       <p class="text-gray-400 text-sm md:text-base mb-2 md:mb-2">Branding / Marketing</p>
                        <div class="flex items-center justify-center">
                            <div class="flex gap-4">
+                                <a href="https://github.com/LiamRJ" target="_blank" class="text-gray-400 hover:text-gray-500 active:text-gray-600 transition duration-100">
+                                   <Icon name="fa6-brands:github" class="h-5 w-5" />
+                               </a>
                                <a href="https://www.linkedin.com/in/liam-runström-jardbring-324218241" target="_blank" class="text-gray-400 hover:text-gray-500 active:text-gray-600 transition duration-100">
                                    <Icon name="fa6-brands:linkedin-in" class="h-5 w-5" />
                                </a>
-                               <a href="https://github.com/LiamRJ" target="_blank" class="text-gray-400 hover:text-gray-500 active:text-gray-600 transition duration-100">
+                           </div>
+                       </div>
+                   </div>
+               </div>
+               <div class="bg-gray-800 pb-5 rounded-2xl items-center justify-center text-center">
+                   <div class="overflow-hidden rounded-lg mb-2 sm:mb-4">
+                       <img src="https://cdn.discordapp.com/avatars/792587261828530187/a_8efd2572baaebf2f8b489fa3e360b02b.png" class="w-[400px] h-[400px] object-center" />
+                   </div>
+                   <div>
+                       <div class="text-indigo-500 md:text-lg font-bold">
+                           <div class="flex items-center text-center justify-center gap-x-2 lg:gap-x-2"><img src="/img/sweden.png" class="h-[20px] w-[30px]" /> Ewwe K.</div>
+                       </div>
+                       <p class="text-gray-400 text-sm md:text-base mb-2 md:mb-2">Emotional Damage</p>
+                       <div class="flex items-center justify-center">
+                           <div class="flex gap-4">
+                                <a href="https://github.com/LiamRJ" target="_blank" class="text-gray-400 hover:text-gray-500 active:text-gray-600 transition duration-100">
                                    <Icon name="fa6-brands:github" class="h-5 w-5" />
+                               </a>
+                               <a href="https://www.linkedin.com/in/liam-runström-jardbring-324218241" target="_blank" class="text-gray-400 hover:text-gray-500 active:text-gray-600 transition duration-100">
+                                   <Icon name="fa6-brands:linkedin-in" class="h-5 w-5" />
                                </a>
                            </div>
                        </div>
